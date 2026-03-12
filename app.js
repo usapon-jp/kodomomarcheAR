8a0b313a1f55a408a94e6b5e9161b52eb9416bc7
usapon <yoshida.fumio230@gmail.com>
2026-03-12 23:08:17 +0900

    Refine home layout and camera framing

--- app.js
+++ app.js
@@ -733,7 +733,12 @@ async function startArCamera(mode = currentFacingMode) {
 
   try {
     currentStream = await navigator.mediaDevices.getUserMedia({
-      video: { facingMode: { ideal: mode }, width: { ideal: 720 }, height: { ideal: 1280 } },
+      video: {
+        facingMode: { ideal: mode },
+        width: { ideal: 640 },
+        height: { ideal: 480 },
+        aspectRatio: { ideal: 4 / 3 }
+      },
       audio: false
     });
     video.srcObject = currentStream;
